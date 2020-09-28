20b5b1d33d16358cd9067699f81c2dc79137101f
psyMafia <51231411+psyMafia@users.noreply.github.com>
2020-09-28 14:37:28 +0300

    Updates

---
+++ precache-manifest.18e911aee03b9e5a80c83095bd3a7a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292374c56d99291da56ce0c434b15157",
+    "url": "/ImageSearchApp/index.html"
+  },
+  {
+    "revision": "c8291e018ed23c3f5afe",
+    "url": "/ImageSearchApp/static/css/main.650a0f53.chunk.css"
+  },
+  {
+    "revision": "07fa8f14d48d474c5a0f",
+    "url": "/ImageSearchApp/static/js/2.45a25d69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ImageSearchApp/static/js/2.45a25d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8291e018ed23c3f5afe",
+    "url": "/ImageSearchApp/static/js/main.854e054d.chunk.js"
+  },
+  {
+    "revision": "92e01c0cf7a197fbea54",
+    "url": "/ImageSearchApp/static/js/runtime-main.6be7953c.js"
+  }
+]);
